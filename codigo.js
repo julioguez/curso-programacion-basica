6498a9dd09e81ce11d3b734f9864bf24bde31a7c
Julio Tomas Rodríguez <julioguez@gmail.com>
2014-08-04 19:57:59 -0500

    Ejemplos de la primera clase en vivo.
    Ejemplos de la primera clase en vivo.

---
+++ codigo.js
@@ -0,0 +1,14 @@
+var peso;
+peso = prompt("¿Cuál es tu peso cacheton?", "65");
+alert (peso);
+
+
+
+//esto es un comentario en javascript
+// alert("Hola mada, esto esta muy fail!!!");
+
+//  var nombre = "Julio";
+//var apellido = "Camaney";
+// var edad = 28;
+
+// alert(nombre + " " + apellido + "\n" + edad + " " + "años.");
